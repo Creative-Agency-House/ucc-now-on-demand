2a54ce8232f3a8557c0e0967f83c2ab5f5667fad
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2026-07-15 21:44:23 +0500

    bump

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
-aws s3 sync frontend/ s3://xx --delete --profile ho7
-aws cloudfront create-invalidation --distribution-id xx --paths "/*" --profile ho7
+echo "Building frontend..."
+cd frontend
+npm run build
+cd ..
+echo "Deploying frontend to S3..."
+aws s3 sync frontend/dist s3://uccnowstack-uccnowwebsites3bucket33df281f-j3cquancavxe --delete --profile ho7
+echo "Invalidating CloudFront cache..."
+aws cloudfront create-invalidation --distribution-id ES0UITRLTTZK1 --paths "/*" --profile ho7
